5f409dc6ae2738ef0403c85929f9044959c5da30
Geary-E <geary16.erua@gmail.com>
2023-04-21 14:04:43 -0500

    Updated Dashboard styling

--- src/dashboard.php
+++ src/dashboard.php
@@ -11,6 +11,8 @@ if (isset($_SESSION['researcher_name'])) {
 
 if (isset($_SESSION['person_name'])) {
     $name = $_SESSION['person_name'];
+} else {
+    $name = "______";
 }
 
 ?>
@@ -23,18 +25,54 @@ if (isset($_SESSION['person_name'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style3.css">
     <title>Dashboard</title>
 </head>
 
 <body>
-
+<!--
     <div class="container">
-        <div class="content">
-            <h1>Hello <span><?php echo $name ?></span> this is the dashboard</h1>
-            <a href="logout.php" class="btn"> Logout</a>
+-->
+        <div class="dashboard-header">
+            <div class="logo">
+               <h2> Logo </h2> 
+            </div>
+            <div class="buttons">
+                <button class="btn1"> Dashboard </button>
+                <button class="btn2"> Logout </button>
+            </div>
         </div>
-    </div>
+
+<!--</span> -->
+            <h1> Hello <span><?php echo"$name, Welcome to the Dashboard."?></h1><br>
+            
+            <!--
+            <a href="logout.php" class="btn"> Logout</a>
+            -->
+
+    <div class="content">
+            <div class="flex1">
+                Account Page
+                </div>
+
+            <div class="flex2">
+                Support Groups
+            </div>
+            
+            <div class="flex3">
+                Surveys
+            </div>
+
+            <div class="flex4">
+                Studies
+            </div>
+
+            <div class="flex5">
+                Opportunities
+            </div>
+
+        </div>
+
 </body>
 
 </html>
