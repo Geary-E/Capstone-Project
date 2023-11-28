e01e46b53b1a2a171360df59c4eda24b7c3ec04c
Geary-E <geary16.erua@gmail.com>
2023-11-28 12:20:39 -0600

    Added Images to the dashboard buttons on the dashboard page

--- src/dashboard.php
+++ src/dashboard.php
@@ -32,22 +32,27 @@ $name=nameType();
     <div class="content">
       
         <a class="flex1" href="accountPage.php">  <!-- Account Page button -->
+            <img class="user-account-pic" src="./images/account_pic.png" alt="User Account"><br><br> <!-- Image associated with account settings -->
             Account Page 
         </a>
 
         <a class="flex2" href="supportGroup.php">  <!-- Support Group button -->
+            <img class="support-group-pic" src="./images/support_group_pic.jpg" alt="Support Groups"><br><br> <!-- Image associated with support groups -->
             Support Groups
         </a>
 
         <a class="flex3" href="survey.php">   <!-- Survey button -->
+            <img class="survey-pic" src="./images/surveys.png" alt="Surveys"><br><br> <!-- Image associated with surveys -->
             Surveys
         </a>
 
         <a class="flex4" href="study.php">  <!-- Studies button -->
+            <img class="studies-pic" src="./images/studies.png" alt="Studies"><br><br> <!-- Image associated with studies -->
             Studies
         </a>	
 
         <a class="flex5" href="opportunity.php">  <!-- Opportunities button -->
+            <img class="opportunity-pic" src="./images/opportunities_pic.png" alt="Opportunities"><br><br> <!-- Image associated with opportunities -->
             Opportunities
         </a>	
     </div>
